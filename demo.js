6b27e1d8312128f8d8390172b0bce1d63face676
Daniel Sierpinski <siery@trixxxy>
2018-02-28 02:42:56 +0100

    Demo version commit

---
+++ demo.js
@@ -0,0 +1,27 @@
+/**
+ * Test comment
+ */
+
+Math.pow(10, 3);
+bar.baz;
+
+
+function square(n) {
+    return n * n;
+}
+
+square(23.3);
+
+skewer.log("Hello, World");
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!isset($_POST['login']) && isset($_POST['passwd']))
+
+$login = $_POST['login'];
+$passwd = $_POST['passwd'];
+
+
+    
+
+?>
